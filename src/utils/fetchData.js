be0c3d2893c1cbc1da14c8e8a7c9c78ffb6a129e
salimbh <sloomabenhalima95@gmail.com>
2022-12-21 00:26:40 +0900

    fetch data

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,16 +1,16 @@
 export const exerciseOptions = {
-  method: 'GET',
+  method: "GET",
   headers: {
-    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+    "X-RapidAPI-Key": "3852c528d2msh185ff347614fc16p1bdc55jsndc26aa714df1",
   },
 };
 
 export const youtubeOptions = {
-  method: 'GET',
+  method: "GET",
   headers: {
-    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
-    'X-RapidAPI-Key': 'f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85',
+    "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
+    "X-RapidAPI-Key": "'f81d5c7777msh2d926f7bd8e2c22p1af665jsn76a239cf9aaa'",
   },
 };
 
