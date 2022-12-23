94a4f24e2ffc4cb68ddc862ad68d44aeebaa1d43
pej.benhalima <pej.benhalima@gmail.com>
2022-12-23 14:42:27 +0900

    continue with details

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -10,7 +10,7 @@ export const youtubeOptions = {
   method: "GET",
   headers: {
     "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
-    "X-RapidAPI-Key": "'f81d5c7777msh2d926f7bd8e2c22p1af665jsn76a239cf9aaa'",
+    "X-RapidAPI-Key": "3852c528d2msh185ff347614fc16p1bdc55jsndc26aa714df1",
   },
 };
 
