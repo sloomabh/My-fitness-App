8d0fc86d6bc2e25e6f2ad2a68b91fdfcbf33da66
pej.benhalima <pej.benhalima@gmail.com>
2023-02-25 16:43:31 +0900

    fix fetch data

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -2,7 +2,7 @@ export const exerciseOptions = {
   method: "GET",
   headers: {
     "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
-    "X-RapidAPI-Key": "8132ce5520msh455c0f098bd4831p12a3f3jsne961f444b167",
+    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
   },
 };
 
@@ -10,13 +10,15 @@ export const youtubeOptions = {
   method: "GET",
   headers: {
     "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
-    "X-RapidAPI-Key": "8132ce5520msh455c0f098bd4831p12a3f3jsne961f444b167",
+    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
   },
 };
-
+//console.log(process.env.REACT_APP_RAPID_API_KEY);
 export const fetchData = async (url, options) => {
   const res = await fetch(url, options);
   const data = await res.json();
 
   return data;
 };
+
+/*  process.env.REACT_APP_RAPID_API_KEY */
